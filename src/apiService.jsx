f98a00cee9a6ae9a815c953751ec7f1b1af117d4
guna-sekar-msu <manogara@msu.edu>
2026-01-29 20:52:45 -0800

    Update API base URL in apiService

--- src/apiService.jsx
+++ src/apiService.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // const API_BASE_URL = 'https://cors-dashboard-backend-2.vercel.app';
 // Local Host URL
-const API_BASE_URL = 'http://127.0.0.1:9000';
+// const API_BASE_URL = 'http://127.0.0.1:9000';
+const API_BASE_URL = 'http://35.9.242.16:9000';
 
 
 const sendJsonData = (inputData) => {
