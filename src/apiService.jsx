35bd70231e0f9603f2cf5ac87f19f39e5fd65d9b
guna-sekar-msu <manogara@msu.edu>
2026-04-02 00:15:50 -0700

    Update API_BASE_URL to new endpoint

--- src/apiService.jsx
+++ src/apiService.jsx
@@ -3,7 +3,8 @@ import axios from 'axios';
 // const API_BASE_URL = 'https://cors-dashboard-backend-2.vercel.app';
 // Local Host URL
 // const API_BASE_URL = 'http://127.0.0.1:9000';
-const API_BASE_URL = 'http://35.9.242.16:9000';
+// const API_BASE_URL = 'http://35.9.242.16:9000';
+const API_BASE_URL = 'https://cors.geodesy.qual.ees.msu.edu';
 
 
 const sendJsonData = (inputData) => {
